75c5d22d08fa6ebdcff115a2fabb40fdb9839847
qlauret <lauret.consultant.informatique@gmail.com>
2023-12-22 12:42:36 +0100

    ajout module users

--- quizzy/apps/quizzy/src/app/app.module.ts
+++ quizzy/apps/quizzy/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FirebaseModule } from 'nestjs-firebase';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
@@ -10,6 +10,7 @@ import { FirebaseModule } from 'nestjs-firebase';
       googleApplicationCredential:
         'apps/quizzy/src/assets/quizzy-12a74-firebase-adminsdk-vulpk-38378d1f85.json'
     }),
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService]
